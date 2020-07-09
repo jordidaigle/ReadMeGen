788a9158ca930462c49599676d80d43d33c982b6
jordidaigle <jdaigle992@gmail.com>
2020-07-09 17:11:17 -0500

    First trial of Generator

--- Index.js
+++ Index.js
@@ -17,7 +17,7 @@
 
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateMarkdown = require('generateMarkdown.js');
+// const generateMarkdown = require("generateMarkdown.js");
 var badge;
 
 function userInput() {
@@ -105,6 +105,54 @@ function writeToFile(file, data) {
       }
       console.log("Success!");
   })
+}
+
+function generateMarkdown({ username, email, projecttitle, description, installation, usage, testing, license}) {
+  projecttitleDashed = projecttitle.replace(/\s+/g, '-');
+  return `
+  [contributors-shield]: https://img.shields.io/github/contributors/${username}/${projecttitleDashed}.svg?style=flat-square
+  [contributors-url]: https://github.com/${username}/${projecttitleDashed}/graphs/contributors
+  [forks-shield]: https://img.shields.io/github/forks/${username}/${projecttitleDashed}.svg?style=flat-square
+  [forks-url]: https://github.com/${username}/${projecttitleDashed}/network/members
+  [stars-shield]: https://img.shields.io/github/stars/${username}/${projecttitleDashed}.svg?style=flat-square
+  [stars-url]: https://github.com/${username}/${projecttitleDashed}/stargazers
+  [issues-shield]: https://img.shields.io/github/issues/${username}/${projecttitleDashed}.svg?style=flat-square
+  [issues-url]: https://github.com/${username}/${projecttitleDashed}/issues
+  [license-shield]: https://img.shields.io/github/license/${username}/${projecttitleDashed}.svg?style=flat-square
+  [license-url]: https://github.com/${username}/${projecttitleDashed}/blob/master/LICENSE.txt
+  [![Contributors][contributors-shield]][contributors-url] [![Forks][forks-shield]][forks-url] [![Stargazers][stars-shield]][stars-url] [![Issues][issues-shield]][issues-url] [![License.txt][license-shield]][license-url]
+  # ${projecttitleDashed}
+  ## Description
+  ${description}
+  ## Table of Contents
+  - [Title](#title)
+  - [Description](#description)
+  - [Table of Contents](#table-of-contents)
+  - [Screenshots](#screenshots)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Testing](#testing)
+  - [Additional Information](#additional-information)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Questions and Feedback](#questions-and-feedback)
+  ## Installation
+  Download (and unpack) or clone the repo, then using a CLI run the ${installation} command.
+  ## Usage
+  Run the application with the CLI command ${usage} and follow the prompts.
+  ## Testing
+  Run the tests with the CLI command ${testing}.
+  ## License
+  This project is licensed under the ${license} License - see the [LICENSE.txt](https://github.com/${username}/${projecttitleDashed}/blob/master/LICENSE.txt) file for details
+  ## Contributing
+  Please read [CONTRIBUTING.md](https://github.com/${username}/${projecttitleDashed}/blob/master/CONTRIBUTING.md) for details on the code of conduct, and the process for submitting pull requests.
+  ## Questions and Feedback
+  Please contact me using one of the following:
+  |---|---|
+  - | Github: [${username}](https://gist.github.com/${username}) |
+  |---|---|
+  - Email: ${email}
+  `
 }
 
 userInput()
---
+++ README.md
@@ -0,0 +1,44 @@
+
+  [contributors-shield]: https://img.shields.io/github/contributors/github/REadMe.svg?style=flat-square
+  [contributors-url]: https://github.com/github/REadMe/graphs/contributors
+  [forks-shield]: https://img.shields.io/github/forks/github/REadMe.svg?style=flat-square
+  [forks-url]: https://github.com/github/REadMe/network/members
+  [stars-shield]: https://img.shields.io/github/stars/github/REadMe.svg?style=flat-square
+  [stars-url]: https://github.com/github/REadMe/stargazers
+  [issues-shield]: https://img.shields.io/github/issues/github/REadMe.svg?style=flat-square
+  [issues-url]: https://github.com/github/REadMe/issues
+  [license-shield]: https://img.shields.io/github/license/github/REadMe.svg?style=flat-square
+  [license-url]: https://github.com/github/REadMe/blob/master/LICENSE.txt
+  [![Contributors][contributors-shield]][contributors-url] [![Forks][forks-shield]][forks-url] [![Stargazers][stars-shield]][stars-url] [![Issues][issues-shield]][issues-url] [![License.txt][license-shield]][license-url]
+  # REadMe
+  ## Description
+  Generates ReadMe's
+  ## Table of Contents
+  - [Title](#title)
+  - [Description](#description)
+  - [Table of Contents](#table-of-contents)
+  - [Screenshots](#screenshots)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Testing](#testing)
+  - [Additional Information](#additional-information)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Questions and Feedback](#questions-and-feedback)
+  ## Installation
+  Download (and unpack) or clone the repo, then using a CLI run the Nope command.
+  ## Usage
+  Run the application with the CLI command Some and follow the prompts.
+  ## Testing
+  Run the tests with the CLI command None.
+  ## License
+  This project is licensed under the la License - see the [LICENSE.txt](https://github.com/github/REadMe/blob/master/LICENSE.txt) file for details
+  ## Contributing
+  Please read [CONTRIBUTING.md](https://github.com/github/REadMe/blob/master/CONTRIBUTING.md) for details on the code of conduct, and the process for submitting pull requests.
+  ## Questions and Feedback
+  Please contact me using one of the following:
+  |---|---|
+  - | Github: [github](https://gist.github.com/github) |
+  |---|---|
+  - Email: jdaigle
+  
